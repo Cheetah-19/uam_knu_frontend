f61d85e0c006fb61f5d9fe86e07f56fd4a58f8c5
Seung Woon <113246980+Usimth@users.noreply.github.com>
2024-05-17 13:39:24 +0900

    Chore : Set login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Start from '../src/pages/Start';
+import Login from '../src/pages/Login';
 
 function App() {
   return (
     <Router>
-      <Start />
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+    return (
+        <div>
+        <h1>로그인 페이지</h1>
+        </div>
+    );
+}
+
+export default Login;
