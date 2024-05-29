a1f7aed9b2d12a6ee7c11f75407fc7824b4f289a
Lucerna00 <pjs3859811@naver.com>
2024-05-30 02:57:44 +0900

    Feat: Add selected class to buttons in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import logo from "../assets/logo.png"
-import { useNavigate } from "react-router-dom";
 import "../styles/App.css";
 
 const Header = () => {
     const navigate = useNavigate();
+    const location = useLocation(); // 현재 위치 정보
+
+    // 현재 경로에 따라 selected 클래스를 추가
+    const isSelected = (path) => {
+        return location.pathname === path ? "selected" : "";
+    };
 
     return (
         <header>
-            <img src={logo}/>
+            <img src={logo} alt="logo" />
             <div>
-                <div className="header-btn" onClick={() => navigate("/start")}>
+                <div className={`header-btn ${isSelected('/start')}`} onClick={() => navigate("/start")}>
                     최적화하기
                 </div>
-                <div className="header-btn selected" onClick={() => navigate("/user")}>
+                <div className={`header-btn ${isSelected('/user')}`} onClick={() => navigate("/user")}>
                     마이페이지
                 </div>
             </div>
