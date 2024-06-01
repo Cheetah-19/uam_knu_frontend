cc99c5c98573efad4c38b1d9c71a3e90324aea2c
Apoliasm <syjsir@gmail.com>
2024-06-01 21:19:31 +0900

    FEAT : Add RESTAPI for adding new vertiport

--- src/components/modal/AddVertiport.js
+++ src/components/modal/AddVertiport.js
@@ -1,5 +1,7 @@
 import "../../styles/App.css"
 import { useState } from "react";
+import { privateApi } from "../Functions";
+import React from 'react';
 function AddVertiport(props) {
 
     const [maxFatoUAM, setMaxFatoUAM] = useState('');
@@ -8,38 +10,126 @@ function AddVertiport(props) {
     const [maxGateUAM, setMaxGateUAM] = useState('');
     const [maxGatePassengers, setMaxGatePassengers] = useState('');
     const [selectedVertiport, setSelectedVertiport] = useState(null);
+    const [addVertiport, setAddvertiport ] = useState(true);
     const handleVertiportSelect = (selectedVertiport) => {
         console.log('선택된 버티포트:', selectedVertiport);
         setSelectedVertiport(selectedVertiport);
         if (selectedVertiport) {
+          setSelectedVertiport(selectedVertiport.name.toString());
           setMaxFatoUAM(selectedVertiport.fato.toString());
           setMaxPathInUAM(selectedVertiport.path_in.toString());
           setMaxPathOutUAM(selectedVertiport.path_out.toString());
           setMaxGateUAM(selectedVertiport.gate.toString());
           setMaxGatePassengers(selectedVertiport.waiting_room.toString());
+          setAddvertiport(false);
         //   document.getElementById('dropdown-left').innerText = selectedVertiport.name;
         } else {
+          setSelectedVertiport(null);
           setMaxFatoUAM('');
           setMaxPathInUAM('');
           setMaxPathOutUAM('');
           setMaxGateUAM('');
           setMaxGatePassengers('');
+          setAddvertiport(true);
         }
       };
-    const addVertiportsetting = () => {
-        setMaxFatoUAM('');
-        setMaxPathInUAM('');
-        setMaxPathOutUAM('');
-        setMaxGateUAM('');
-        setMaxGatePassengers('');
 
-    }
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        switch (name) {
+          case 'selectedVertiport':
+            setSelectedVertiport(value);
+            break;
+          case 'maxFatoUAM':
+            setMaxFatoUAM(value);
+            break;
+          case 'maxPathInUAM':
+            setMaxPathInUAM(value);
+            break;
+          case 'maxPathOutUAM':
+            setMaxPathOutUAM(value);
+            break;
+          case 'maxGateUAM':
+            setMaxGateUAM(value);
+            break;
+          case 'maxGatePassengers':
+            setMaxGatePassengers(value);
+            break;
+          default:
+            break;
+        }
+      };
     console.log(props.vertiports)
+
+    const closeModal = (e) => {
+        props.closeModal();
+    };
+    
+    const handlePost = async (event) => {
+        event.preventDefault(); // Prevent the default form submission
+        try {
+            const response = await privateApi.post("/vertiports",{
+                "name": selectedVertiport,
+                "fato": maxFatoUAM,
+                "path_in": maxPathInUAM,
+                "gate": maxGateUAM,
+                "path_out": maxPathOutUAM,
+                "waiting_room": maxGatePassengers
+            },{
+                withCredentials: true
+            });
+            console.log(response);
+            if (response.status === 200) {
+                alert("새 버티포트 추가!");
+                // Handle successful login (e.g., save token, redirect)
+                // closeModal();
+            }
+        } catch (error) {
+            console.log(error)
+            if (error.response && error.response.status === 400) {
+                alert("버티포트 추가 실패 : 400");
+            } else {
+                alert("버티포트 실패 : other");
+            }
+            console.error("Login failed:", error.response ? error.response.data : error.message);
+        }
+    };
+
+    
+
+    const handleUpdate = async (event) => {
+        event.preventDefault(); // Prevent the default form submission
+        try {
+            const response = await privateApi.post("/vertiports",{
+                "name": selectedVertiport,
+                "fato": maxFatoUAM,
+                "path_in": maxPathInUAM,
+                "gate": maxGateUAM,
+                "path_out": maxPathOutUAM,
+                "waiting_room": maxGatePassengers
+            });
+            console.log(response);
+            if (response.status === 200) {
+                alert("새 버티포트 추가!");
+                // Handle successful login (e.g., save token, redirect)
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                alert("버티포트 추가 실패 : 400");
+            } else {
+                alert("버티포트 추가 실패 : other");
+            }
+            console.error("Login failed:", error.response ? error.response.data : error.message);
+        }
+    };
+
+
+
     return (
 
         <div className="Addmodal">
             <span className="Addmodal-left">
-                <button className="Addbutton" onClick={() => addVertiportsetting()}>
+                <button className="Addbutton" onClick={() => handleVertiportSelect()}>
                     <span className="plusmark">+</span>
                     <span className="plustext">버티포트 추가</span>
                 </button>
@@ -63,6 +153,18 @@ function AddVertiport(props) {
             <span className="Addmodal-right">
                 <div className="constant-settings" id='modal'>
                     <h5>버티포트 수용량 관리</h5>
+                    <div className="constant-input-container">
+                        <label>{addVertiport === true ? "새 버티포트 이름" : "현재 선택한 버티포트"}</label>
+                        <input
+                            type="text"
+                            name="selectedVertiport"
+                            value={selectedVertiport === null ? "":selectedVertiport }
+                            className="constant-input"
+                            onChange={handleInputChange}
+                            disabled={addVertiport === true ? false: true }
+                        ></input>
+                    </div>
+                    
                     {props.constants && props.constants.map(input => (
                     <div className="constant-input-container" key={input.name}> {/* 수정 */}
                     <label>{input.label}</label>
@@ -71,12 +173,13 @@ function AddVertiport(props) {
                         name={input.name}
                         value={input.name.includes('max') ? eval(input.name) : ''}
                         className={input.className}
+                        onChange={handleInputChange}
                     />
                     </div>
                     ))}
                 </div>
                 <div className="constant-settings" id='button'>
-                    <button className="post-button">수정</button>
+                    <button className="post-button" onClick={addVertiport === true ? handlePost : handleUpdate}>수정</button>
 
                 </div>
             </span>
